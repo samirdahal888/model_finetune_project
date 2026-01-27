8ef2a6cab46ae85ae859dd2f583752b17a244cb0
samir dahal <samirdahal877@gmail.com>
2026-01-27 09:31:16 +0545

    creating training pipeline for model training

---
+++ script/script.py
@@ -0,0 +1,22 @@
+from src.data_loader import DataLoader
+from src.model import ModelManager
+from src.trainer import ModelTrainer
+
+
+def create_training_pipeline():
+    dataloader = DataLoader()
+
+    data = dataloader.prepare_dataset()
+
+    model_manager = ModelManager()
+
+    model, tokenizer = model_manager.create_model()
+
+    trainer = ModelTrainer(model, tokenizer, data)
+
+    trainer.train()
+    
+
+
+if __name__ == "__main__":
+    create_training_pipeline()
