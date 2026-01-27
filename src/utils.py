582d903eb8b05d56ecb64e914ccbe28e4ffc2282
samir dahal <samirdahal877@gmail.com>
2026-01-27 09:29:59 +0545

    utils function for get_device

---
+++ src/utils.py
@@ -0,0 +1,12 @@
+import torch
+
+
+def get_device():
+    if torch.cuda.is_available():
+        device = torch.device("cuda")
+        print(f"Using GPU: {torch.cuda.get_device_name(0)}")
+    else:
+        device = torch.device("cpu")
+        print("Using CPU (training will be slower)")
+
+    return device
